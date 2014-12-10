97d498fca908f4da597a848e16216df0f4d498a5
Brandon Brown <brandon@brandonb.io>
2014-12-10 15:07:47 -0800

    Cleanup tests

--- test/embeds.js
+++ test/embeds.js
@@ -1,6 +1,6 @@
-var oembed    = require("../oembed-io");
-var should    = require('chai').should();
-var expect    = require('chai').expect;
+var oembed = require("../oembed-io");
+var should = require('chai').should();
+var expect = require('chai').expect;
 
 describe('oEmbed.io functions test', function() {
 
@@ -9,8 +9,6 @@ describe('oEmbed.io functions test', function() {
       oembed.get('https://www.youtube.com/watch?v=cR_GVBdZF_M', function(err, code, body) {
         expect(code).to.be.equal(200);
         expect(body).to.be.an('object');
-        // console.dir(code);
-        // console.dir(body);iter
         done();
       });
     });
@@ -27,25 +25,12 @@ describe('oEmbed.io functions test', function() {
       });
     });
   });
-  // describe('#oembed.get() with args', function() {
-  //   it('should return a json object', function(done) {
-  //     oembed.get('https://www.youtube.com/watch?v=cR_GVBdZF_M', args, function(err, code, body) {
-  //       // expect(body.type).to.be.equal('user.list');
-  //       // expect(body.users).to.be.an('object');
-  //       console.dir(code);
-  //       console.dir(body);
-  //       done();
-  //     });
-  //   });
-  // });
 
   describe('#oembed.providers()', function() {
     it('should return an array', function(done) {
       oembed.providers(function(err, code, body) {
         expect(code).to.be.equal(200);
         expect(body).to.be.an('array');
-        // console.dir(code);
-        // console.dir(body);
         done();
       });
     });
